31711e6e17fb37b8e5e628befbbd560d15fa12c2
szewczykm <szewczykm94@gmail.com>
2020-05-21 01:16:16 +0200

    Implemented Spark core job example

--- src/main/java/com/szewczykm/spark/architecture/Main.java
+++ src/main/java/com/szewczykm/spark/architecture/Main.java
@@ -1,8 +1,25 @@
 package com.szewczykm.spark.architecture;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
 /**
  * [REF 1.1]
  */
 
 public class Main {
+    public static void main(String[] args) {
+        SparkConf config = new SparkConf().setAppName("Longest ascending subsequence.");
+        JavaSparkContext context = new JavaSparkContext(config);
+        JavaRDD<Integer> rdd = context.parallelize(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        double sumOfSquaredEvenNumbers = rdd.filter(Main::isEven).map(Math::sqrt).reduce(Double::sum);
+        System.out.println("Spark Job execution result: " + sumOfSquaredEvenNumbers);
+    }
+
+    private static boolean isEven(int value) {
+        return (value % 2) == 0;
+    }
 }
