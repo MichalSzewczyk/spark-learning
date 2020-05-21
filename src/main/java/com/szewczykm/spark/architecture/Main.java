acdb9d06b46e2d17b7e4926f8940156ea64855f5
szewczykm <szewczykm94@gmail.com>
2020-05-21 23:00:06 +0200

    Implemented Spark core job example

--- src/main/java/com/szewczykm/spark/architecture/Main.java
+++ src/main/java/com/szewczykm/spark/architecture/Main.java
@@ -4,22 +4,44 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * [REF 1.1]
  */
 
 public class Main {
+    // Replace with your hadoop home dir
+    private static final String HADOOP_HOME_DIR = "D:\\Coding\\hadoop-winutil";
+    private static final int WHILE = 1000;
+
     public static void main(String[] args) {
-        SparkConf config = new SparkConf().setAppName("Longest ascending subsequence.");
-        JavaSparkContext context = new JavaSparkContext(config);
-        JavaRDD<Integer> rdd = context.parallelize(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
-        double sumOfSquaredEvenNumbers = rdd.filter(Main::isEven).map(Math::sqrt).reduce(Double::sum);
+        JavaSparkContext context = configureSparkContext();
+
+        JavaRDD<Integer> rdd = context.parallelize(new ArrayList<>(IntStream.range(1, 10000000).boxed().collect(Collectors.toList())));
+        double sumOfSquaredEvenNumbers = rdd.filter(Main::isEven).map(Math::sqrt).map(Main::sleepForAWhile).reduce(Double::sum);
         System.out.println("Spark Job execution result: " + sumOfSquaredEvenNumbers);
+    }
+
+    private static JavaSparkContext configureSparkContext() {
+        System.setProperty("hadoop.home.dir", HADOOP_HOME_DIR);
+        SparkConf config = new SparkConf().setAppName("Example spark application.");
+        config.set("spark.master", "local");
+        return new JavaSparkContext(config);
     }
 
     private static boolean isEven(int value) {
         return (value % 2) == 0;
+    }
+
+    private static <T> T sleepForAWhile(T value) {
+        try {
+            Thread.sleep(WHILE);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return value;
     }
 }
